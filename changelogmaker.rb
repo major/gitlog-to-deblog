ba39bab200aa8e7dea3f348a6f16f0c2841302a9
rackerhacker <major@mhtx.net>
2011-12-08 07:58:14 -0600

    Added documentation and some code to make a temporary tag for repos with no tags at all.

--- changelogmaker.rb
+++ changelogmaker.rb
@@ -1,29 +1,16 @@
 #!/usr/bin/ruby
 require 'erb'
 
+# Determines package name from the origin url on github. It's hackish, but it
+# works (mostly).
 def pkgname
   originurl = `git config --get remote.origin.url`.strip
   _, pkgname = originurl.match(/\/([a-z0-9\-_]+).git/i).to_a
   pkgname
 end
 
-PKGNAME = pkgname
-
-=begin
-# Required format for Debian changelogs
-# gathered from http://www.debian.org/doc/debian-policy/ch-source.html
-
-package (version) distribution(s); urgency=urgency
-      [optional blank line(s), stripped]
-  * change details
-    more change details
-      [blank line(s), included in output of dpkg-parsechangelog]
-  * even more change details
-      [optional blank line(s), stripped]
- -- maintainer name <email address>[two spaces]  date
-
-=end
-
+# Accepts a hash of git log data and returns a properly formatted debian 
+# changelog entry.
 def debchangelog(logdata)
   template = <<-EOF
 <%=PKGNAME%> (<%=logdata[:tag]%>) unstable; urgency=low
@@ -36,13 +23,52 @@ EOF
   ERB.new(template).result(binding)
 end
 
-IO.popen(%{git log -n 25 --pretty=format:'hash: %H%nname: %aN <%aE>%ndate: %cD%nsubj: %s'}).readlines.each_slice(4) do |chunk|
+# Checks to see if the repository has any tags already.
+def repo_has_tag?
+  `git describe --tags 2>&1`
+  return ($? == 0)? true : false
+end
+
+# If the repository has no tags, we need to make one called 'initial' so we 
+# can get some kind of versioning number for the changelog.
+def make_temporary_tag
+  firstcommit = `git log --format=%H | tail -1`.strip
+  `git tag initial #{firstcommit}`
+end
+
+# Removes the tag we added if the repo had no tags.
+def cleanup_temporary_tag
+  `git tag -d initial`
+end
+
+# Basic setup before we start the loop
+PKGNAME = pkgname.downcase
+if repo_has_tag?
+  # the repo has at least one tag already
+  DOTAGCLEANUP = false
+else
+  # the repo has no tags, so we need to make one
+  DOTAGCLEANUP = true
+  make_temporary_tag
+end
+
+# Loop through the git log output and grab four lines at a time to parse.
+gitlogcmd = %{git log -n 25 --pretty=format:'hash: %H%nname: %aN <%aE>%ndate: %cD%nsubj: %s'}
+IO.popen(gitlogcmd).readlines.each_slice(4) do |chunk|
+
   temphash = {}
+
+  # split each line on the first colon and use what's on the left as the 
+  # symbols within the hash
   chunk.map { |line| line.split(/: /,2) }.each do |type, data|
     temphash[type.to_sym] = data.strip
   end
+
+  # dig up the most recent tag which contains the commit
   temphash[:tag] = `git describe --tags #{temphash[:hash]}`.strip
-  
+
   puts debchangelog(temphash)
-  
 end
+
+# If we added a temporary tag, let's remove it
+cleanup_temporary_tag if DOTAGCLEANUP
--- readme.md
+++ readme.md
@@ -2,6 +2,7 @@ WORK IN PROGRESS.
 
 This script converts a git log to a debian changelog suitable for use with deb packaging.  I'm not a DD and I can't verify that the changelog will work for all use cases.
 
-**Your git repo must have at least one tag for this script to work.**  Customize the :tag symbol if you go without tags.
+If your repo has no tags, the script will make a temporary tag on your first
+commit, output the changelog, and then remove the temporary tag.  To avoid this functionality, add some tags to your repository.
 
 To use the script, simply drop it into the base directory of your favorite git repository and run it.  It pulls 25 commit messages by default.
